989e5015b2ff94126b5705216da8f8738092539c
cdegiere <cdegiere@tcv.com>
2018-05-07 13:33:34 -0700

    giving up

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='nbparameterise',
+      url='https://github.com/takluyver/nbparameterise',
+      license='MIT',
+      packages=['nbparameterise'],
+      install_requires=['nbconvert', 'astsearch'])
