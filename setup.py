06c38bf106dc01128a7f3b836d6ca82ca0545260
cdegiere <cdegiere@tcv.com>
2018-05-07 14:14:36 -0700

    setup packages

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='nbparameterise',
       version='0.3',
       url='https://github.com/takluyver/nbparameterise',
       license='MIT',
-      packages=['nbparameterise'],
+      packages=find_packages(),
       install_requires=['nbconvert', 'astsearch'])
