485c44ce3ddd7cee1a261c7099a0a3c1ede30a6c
cdegiere <cdegiere@tcv.com>
2018-05-07 13:42:20 -0700

    match version number

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup
 
 setup(name='nbparameterise',
+      version='0.3',
       url='https://github.com/takluyver/nbparameterise',
       license='MIT',
       packages=['nbparameterise'],
